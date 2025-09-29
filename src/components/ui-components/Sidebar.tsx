fbc3b311f06abf9e16fe50198c005b9a4ef42974
Vanessa Pisati <pisati.vanessa@gmail.com>
2025-09-29 21:55:00 +0200

    Created Sidebar Components

--- src/components/ui-components/Sidebar.tsx
+++ src/components/ui-components/Sidebar.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import SidebarCard, { type SidebarProps } from "./SidebarCard";
-import { faUsers, faCreditCard } from "@fortawesome/free-solid-svg-icons";
+import { faUsers, faCreditCard, faChartSimple } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 
 export default function Sidebar() {
 
     const sidebarEl: SidebarProps[] = [
-        {icon: faUsers, label: 'Atleti'},
-        {icon: faCreditCard, label: 'Abbonamenti'}
+        {icon: faChartSimple, label: 'Dashboard', navigateTo: ''},
+        {icon: faUsers, label: 'Atleti', navigateTo: 'athletes'},
+        {icon: faCreditCard, label: 'Abbonamenti', navigateTo: 'subscriptions'}
     ]
 
     return (
         <>
         <div className="min-h-screen py-25 flex flex-col items-center gap-4 w-100 ">
             {sidebarEl.map(el => 
-                <SidebarCard icon={el.icon} label={el.label}/>
+                <SidebarCard icon={el.icon} label={el.label} navigateTo={el.navigateTo}/>
             )}
             
         </div>
--- src/components/ui-components/SidebarCard.tsx
+++ src/components/ui-components/SidebarCard.tsx
@@ -1,16 +1,23 @@
 import { type IconDefinition } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export interface SidebarProps {
     icon: IconDefinition;
     label: string;
-    //navigateTo: string;
+    navigateTo: string;
 }
 
 export default function SidebarCard(props: SidebarProps) {
+    const navigate = useNavigate()
+
+    const handleNavigate = (path: string) => {
+        navigate(`/${path}`)
+    }
+
     return (
-        <div className="w-80 h-15 ps-8 flex items-center rounded-2xl gap-8 transition-all duration-100 ease-in hover:bg-gray-200">
+        <div className="w-80 h-15 ps-8 flex items-center rounded-2xl gap-8 transition-all duration-100 ease-in hover:bg-gray-200 hover:cursor-pointer" onClick={() => handleNavigate(props.navigateTo)}>
             <FontAwesomeIcon icon={props.icon} size="xl" />
             <p className="text-xl font-semibold">{props.label}</p>
         </div>
