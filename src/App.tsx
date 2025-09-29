92ac22ac64f7234f1ad4412f39f09e5af3d24244
Vanessa Pisati <pisati.vanessa@gmail.com>
2025-09-29 21:54:16 +0200

    Updated routes and types

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,26 @@ import AthletesList from './pages/Athletes';
 import AddAthlete from './pages/AddAthlete';
 import Layout from './components/Layout';
 import { AthleteProvider } from './context/AthleteContext';
+import AthleteProfile from './pages/AthleteProfile';
+import Subscriptions from './pages/Subscriptions';
+import { SubscriptionProvider } from './context/SubscriptionsContext';
 
 function App() {
   return (
     <>
       <AthleteProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Layout><Dashboard /></Layout>} />
-            <Route path='/athletes' element={<Layout><AthletesList /></Layout>} />
-            <Route path='/add-athlete' element={<Layout><AddAthlete /></Layout>} />
-          </Routes>
-        </BrowserRouter>
+        <SubscriptionProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path='/' element={<Layout><Dashboard /></Layout>} />
+              <Route path='/athletes' element={<Layout><AthletesList /></Layout>} />
+              <Route path='/athlete/add' element={<Layout><AddAthlete /></Layout>} />
+              <Route path='/athlete/edit/:id' element={<Layout><AddAthlete /></Layout>} />
+              <Route path='/athlete/:id' element={<Layout><AthleteProfile /></Layout>} />
+              <Route path='/subscriptions' element={<Layout><Subscriptions /></Layout>} />
+            </Routes>
+          </BrowserRouter>
+        </SubscriptionProvider>
       </AthleteProvider>
     </>
   )
--- src/types.tsx
+++ src/types.tsx
@@ -1,28 +1,42 @@
-export type SubscriptionType = 'single' | 'month' | 'quaterly';
 export type AdultBelts = 'white' | 'blue' | 'purple' | 'brown' | 'black';
 export type KidsBelts =     'white' | 'greywhite' | 'grey' | 'greyblack' |
 'yellowwhite' | 'yellow' | 'yellowblack' |
 'orangewhite' | 'orange' | 'orangeblack' |
-'greenwhite' | 'green' | 'greenblack' 
+'greenwhite' | 'green' | 'greenblack'; 
+export type AthleteType = 'kid' | 'adult';
+
+export type SubscriptionType = 'month' | 'quarterly';
+export type PaymentType = 'banktransfer' | 'cash' | 'paypal';
+export type EnsuranceType = 'A' | 'B';
+
+
+type AllBelts = AdultBelts | KidsBelts;
 
 export interface Athlete {
     _id: string,
     name: string,
     surname: string,
     birthDate: Date,
+    fiscalCode: string,
     email: string,
     phoneNumber: string,
-    type: string,
-    belt: string,
-    subscriptionId: string
+    type: AthleteType,
+    belt: AllBelts,
+    subscriptionId: string,
+    medicalCertificate: boolean,
+    medicalCertificateExp: Date | null,
+    ensurance: boolean,
+    ensuranceType: EnsuranceType,
+    ensuranceExp: Date
 }
 
 export interface Subscription {
     uid: string,
     date: Date,
-    type: string,
+    type: SubscriptionType,
     amount: number,
     subscriptionExp: Date,
     athleteId: string,
-    notes?: string
+    notes?: string,
+    paymentType: PaymentType
 }
